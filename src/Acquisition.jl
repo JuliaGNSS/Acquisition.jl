adce423ba623f8e9086ad0b95663c7eb391c7837
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2020-02-11 10:59:57 +0100

    Increase performance

--- src/Acquisition.jl
+++ src/Acquisition.jl
@@ -20,11 +20,11 @@ module Acquisition
         integration_time = length(signal) / sample_freq
         doppler_step = 1 / 3 / integration_time
         doppler_steps = -max_doppler:doppler_step:max_doppler
-        cross_corr_powers = power_over_doppler_and_code(S, signal, sat_prn, doppler_steps, sample_freq, interm_freq)
-        signal_power, noise_power, code_index, doppler_index = est_signal_noise_power(cross_corr_powers, doppler_steps, integration_time, sample_freq, get_code_frequency(S))
+        powers = power_over_doppler_and_code(S, signal, sat_prn, doppler_steps, sample_freq, interm_freq)
+        signal_power, noise_power, code_index, doppler_index = est_signal_noise_power(powers, doppler_steps, integration_time, sample_freq, get_code_frequency(S))
         CN0 = 10 * log10(signal_power / noise_power / code_period / 1.0Hz)
         doppler = (doppler_index - 1) * doppler_step - max_doppler
-        AcquisitionResults(doppler, (code_index - 1) / (sample_freq / get_code_frequency(S)), CN0, cross_corr_powers, first(doppler_steps) / 1.0Hz:step(doppler_steps) / 1.0Hz:last(doppler_steps) / 1.0Hz)
+        AcquisitionResults(doppler, (code_index - 1) / (sample_freq / get_code_frequency(S)), CN0, powers, first(doppler_steps) / 1.0Hz:step(doppler_steps) / 1.0Hz:last(doppler_steps) / 1.0Hz)
     end
 
     function power_over_doppler_and_code(::Type{S}, signal, sat_prn, doppler_steps, sample_freq, interm_freq) where S <: AbstractGNSSSystem
@@ -47,8 +47,10 @@ module Acquisition
     function est_signal_noise_power(power_bins, doppler_steps, integration_time, sample_freq, code_freq)
         samples_per_chip = floor(Int, sample_freq / code_freq)
         signal_noise_power, index = findmax(power_bins)
-        linear_index = size(power_bins, 1) * (index[2] - 1) + index[1]
-        noise_power = median(power_bins[[1:linear_index - 1; linear_index + 1:length(power_bins)]])
+        lower_code_phases = 1:index[1] - samples_per_chip
+        upper_code_phases = index[1] + samples_per_chip:size(power_bins, 1)
+        samples = (length(lower_code_phases) + length(upper_code_phases)) * size(power_bins, 2)
+        noise_power = (sum(power_bins[lower_code_phases,:]) + sum(power_bins[upper_code_phases,:])) / samples
         signal_power = signal_noise_power - noise_power
         signal_power, noise_power, index[1], index[2]
     end
