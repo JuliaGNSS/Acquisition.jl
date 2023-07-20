4f470472fb092fd058f5bc6bd86d0d9c80105e39
Ignatius Rivaldi <minecraft2048@gmail.com>
2023-07-20 11:54:31 +1000

    Use PrecompileTools to precompile Acquisition with common signal types

--- src/Acquisition.jl
+++ src/Acquisition.jl
@@ -65,14 +65,29 @@ include("calc_powers.jl")
 include("est_signal_noise_power.jl")
 include("acquire.jl")
 
-function _precompile_()
-    Base.precompile(Tuple{typeof(Core.kwcall),Core.NamedTuple{(:max_doppler, :coarse_step, :fine_step, :prns, :fft_flag), Tuple{Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}}, Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}}, Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}}, Base.UnitRange{Core.Int64}, Core.UInt32}},Type{CoarseFineAcquisitionPlan},GPSL1{Matrix{Int16}},Int64,Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}}})   # time: 0.34097806
-    isdefined(Acquisition, Symbol("#27#28")) && Base.precompile(Tuple{getfield(Acquisition, Symbol("#27#28")),Vector{ComplexF32},Matrix{Float32},Matrix{ComplexF32}})   # time: 0.16505001
-    isdefined(Acquisition, Symbol("#35#37")) && Base.precompile(Tuple{getfield(Acquisition, Symbol("#35#37")),Matrix{Float32},Matrix{ComplexF32},Int64})   # time: 0.10682159
-    isdefined(Acquisition, Symbol("#35#37")) && Base.precompile(Tuple{getfield(Acquisition, Symbol("#35#37")),Any,Any,Any})   # time: 0.040406585
-    Base.precompile(Tuple{typeof(est_signal_noise_power),Matrix{Float32},Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}},Unitful.Quantity{Core.Int64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}},Nothing})   # time: 0.016112743
-    Base.precompile(Tuple{typeof(est_signal_noise_power),Matrix{Float32},Unitful.Quantity{Core.Float64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}},Unitful.Quantity{Core.Int64, 𝐓^-1, Unitful.FreeUnits{(Hz,), 𝐓^-1, nothing}},Float32})   # time: 0.002283945
+
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    rate = 2.048e6 
+
+    data_ci8 = rand(Complex{Int8},Int(round(rate*0.01)))
+    data_ci16 = rand(Complex{Int16},Int(round(rate*0.01)))
+    data_cf32 = rand(Complex{Float32},Int(round(rate*0.01)))
+    data_cf64 = rand(Complex{Float64},Int(round(rate*0.01)))
+
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+        samplerate = rate*Hz
+        coarse_fine_acquire(GPSL1(),data_ci8[1:2048], samplerate, 1:32; interm_freq=0*Hz, max_doppler=40e3*Hz);
+        coarse_fine_acquire(GPSL1(),data_ci16[1:2048], samplerate, 1:32; interm_freq=0*Hz, max_doppler=40e3*Hz);
+        coarse_fine_acquire(GPSL1(),data_cf32[1:2048], samplerate, 1:32; interm_freq=0*Hz, max_doppler=40e3*Hz);
+        coarse_fine_acquire(GPSL1(),data_cf64[1:2048], samplerate, 1:32; interm_freq=0*Hz, max_doppler=40e3*Hz);
+
+    end
 end
+
 
  
 end
