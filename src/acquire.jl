73e7ca2aaf6575158d2e03a85b1d6111c156cc28
Ignatius Rivaldi <minecraft2048@gmail.com>
2024-01-15 08:39:15 +1100

    Delete commented out codes

--- src/acquire.jl
+++ src/acquire.jl
@@ -33,78 +33,6 @@ function acquire(
     acquire!(acq_plan, signal, prns; interm_freq)
 end
 
-#= function acquire(
-    system::AbstractGNSS,
-    signal,
-    sampling_freq,
-    prns::AbstractVector{<:Integer},
-    time_shift_amt;
-    interm_freq = 0.0Hz,
-    max_doppler = 7000Hz,
-    dopplers = -max_doppler:1/3/(length(signal)/sampling_freq):max_doppler,
-    compensate_doppler_code = false
-)
-    acq_plan = AcquisitionPlan(
-        system,
-        length(signal),
-        sampling_freq;
-        dopplers,
-        prns,
-        fft_flag = FFTW.ESTIMATE,
-        compensate_doppler_code = compensate_doppler_code
-    )
-    acquire!(acq_plan, signal, prns,time_shift_amt; interm_freq)
-end =#
-
-
-#= """
-$(SIGNATURES)
-This acquisition function uses a predefined acquisition plan to accelerate the computing time.
-This will be useful, if you have to calculate the acquisition multiple time in a row.
-"""
-function acquire!(
-    acq_plan::AcquisitionPlan,
-    signal,
-    prns::AbstractVector{<:Integer};
-    interm_freq = 0.0Hz,
-    doppler_offset = 0.0Hz,
-    noise_power = nothing,
-)
-    all(map(prn -> prn in acq_plan.avail_prn_channels, prns)) ||
-        throw(ArgumentError("You'll need to plan every PRN"))
-    code_period = get_code_length(acq_plan.system) / get_code_frequency(acq_plan.system)
-    powers_per_sats, complex_sigs_per_sats =
-        power_over_doppler_and_codes!(acq_plan, signal, prns, interm_freq, doppler_offset)
-    map(powers_per_sats, complex_sigs_per_sats, prns) do powers, complex_sig, prn
-        signal_power, noise_power, code_index, doppler_index = est_signal_noise_power(
-            powers,
-            acq_plan.sampling_freq,
-            get_code_frequency(acq_plan.system), 
-            noise_power,
-        )
-        CN0 = 10 * log10(signal_power / noise_power / code_period / 1.0Hz)
-        doppler =
-            (doppler_index - 1) * step(acq_plan.dopplers) +
-            first(acq_plan.dopplers) +
-            doppler_offset
-        code_phase =
-            (code_index - 1) /
-            (acq_plan.sampling_freq / get_code_frequency(acq_plan.system))
-        AcquisitionResults(
-            acq_plan.system,
-            prn,
-            acq_plan.sampling_freq,
-            doppler,
-            code_phase,
-            CN0,
-            noise_power,
-            powers,
-            complex_sig,
-            (acq_plan.dopplers .+ doppler_offset) / 1.0Hz,
-        )
-    end
-end  =#
-
 """
 $(SIGNATURES)
 This acquisition function uses a predefined acquisition plan to accelerate the computing time.
@@ -141,9 +69,7 @@ function acquire!(
         noncoherent_power_accumulator_buffer = powers_per_sats[noncoherent_power_buffer_idx]
         for (chunk,round_idx) in zip(Iterators.partition(signal[1:(noncoherent_rounds*plan.signal_length)],plan.signal_length), 0:noncoherent_rounds-1)
             acq = acquire_mt!(plan,chunk, [prn]; interm_freq=(intermediate_freq+center_frequency))
-            #power_bin_ncoh3 += acq.power_bins
               #for each dopplers compensate for code drift
-              #for idx1 in 1:length(plan.dopplers)
               for (acc,power_doppler,dop) in zip(eachcol(noncoherent_power_accumulator_buffer),eachcol(acq[1]),plan.dopplers)
                   #CM-ABS algo 3 step 6 eqn 7
                   doppler1 = ustrip(dop)
@@ -207,83 +133,6 @@ function acquire_mt!(
     powers_per_sats = power_over_dopplers_code_mt!(acq_plan,signal,prns,interm_freq,signal_baseband_buffer,signal_baseband_freq_domain_buffer)
     return powers_per_sats
 end
-
-
-#= function acquire!(
-    acq_plan::AcquisitionPlan,
-    signal,
-    prns::AbstractVector{<:Integer},
-    time_shift_amt;
-    interm_freq = 0.0Hz,
-    doppler_offset = 0.0Hz,
-    noise_power = nothing,
-)
-    all(map(prn -> prn in acq_plan.avail_prn_channels, prns)) ||
-        throw(ArgumentError("You'll need to plan every PRN"))
-    code_period = get_code_length(acq_plan.system) / get_code_frequency(acq_plan.system)
-    powers_per_sats, complex_sigs_per_sats =
-        power_over_doppler_and_codes!(acq_plan, signal, prns, interm_freq, doppler_offset,time_shift_amt)
-    map(powers_per_sats, complex_sigs_per_sats, prns) do powers, complex_sig, prn
-        signal_power, noise_power, code_index, doppler_index = est_signal_noise_power(
-            powers,
-            acq_plan.sampling_freq,
-            get_code_frequency(acq_plan.system),
-            noise_power,
-        )
-        CN0 = 10 * log10(signal_power / noise_power / code_period / 1.0Hz)
-        doppler =
-            (doppler_index - 1) * step(acq_plan.dopplers) +
-            first(acq_plan.dopplers) +
-            doppler_offset
-        code_phase =
-            (code_index - 1) /
-            (acq_plan.sampling_freq / get_code_frequency(acq_plan.system))
-        AcquisitionResults(
-            acq_plan.system,
-            prn,
-            acq_plan.sampling_freq,
-            doppler,
-            code_phase,
-            CN0,
-            noise_power,
-            powers,
-            complex_sig,
-            (acq_plan.dopplers .+ doppler_offset) / 1.0Hz,
-        )
-    end
-  end =#
-
-
-  function noncoherent_integrate!(fp, prn, noncoherent_rounds, center_frequency, plan::AcquisitionPlan; intermediate_freq=0, compensate_doppler_code=false)
-    rate = ustrip(plan.sampling_freq)
-  
-    samples_1ms = Int(round(rate * 0.001))
-    power_bin_ncoh3 = zeros(Float32,plan.signal_length,length(plan.dopplers))
-    for (chunk,round_idx) in zip(Iterators.partition(fp[1:(noncoherent_rounds*plan.signal_length)],plan.signal_length), 0:noncoherent_rounds-1)
-      acq = acquire_mt!(plan,chunk, [prn]; interm_freq=(intermediate_freq+center_frequency)*Hz)
-      #power_bin_ncoh3 += acq.power_bins
-        #for each dopplers compensate for code drift
-        #for idx1 in 1:length(plan.dopplers)
-        for (acc,power_doppler,dop) in zip(eachcol(power_bin_ncoh3),eachcol(acq[1]),plan.dopplers)
-            #CM-ABS algo 3 step 6 eqn 7
-            #doppler1 = ustrip(plan.dopplers[idx1])
-            doppler1 = ustrip(dop)
-            if compensate_doppler_code == :negative
-                Ns = sign(doppler1 - center_frequency) * round(round_idx*0.001* ustrip(plan.sampling_freq) * abs(doppler1 - center_frequency)/ustrip(get_center_frequency(plan.system)), RoundNearest)
-                #power_bin_ncoh3[:,idx1] = power_bin_ncoh3[:,idx1] .+ circshift(acq.power_bins[:,idx1],Ns)
-                acc .= acc .+ circshift(power_doppler,-Ns)
-            elseif compensate_doppler_code == :positive
-                Ns = sign(doppler1 - center_frequency) * round(round_idx*0.001* ustrip(plan.sampling_freq) * abs(doppler1 - center_frequency)/ustrip(get_center_frequency(plan.system)), RoundNearest)
-                #power_bin_ncoh3[:,idx1] = power_bin_ncoh3[:,idx1] .+ circshift(acq.power_bins[:,idx1],Ns)
-                acc .= acc .+ circshift(power_doppler,Ns)
-            else
-                acc .= acc .+ power_doppler
-            end
-        end
-
-    end
-    return power_bin_ncoh3  
-  end
 
 function acquire!(
     acq_plan::CoarseFineAcquisitionPlan,
