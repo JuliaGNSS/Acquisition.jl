6d160178ee7b244aca43307e1735178108cd2b91
Soeren Schoenbrod <soeren.schoenbrod@outlook.de>
2025-06-02 23:03:52 +0200

    test: more performant test (#19)
    Pull allocations out of acquisition.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,6 +2,7 @@ using BenchmarkTools
 using Unitful: Hz
 using Acquisition
 using GNSSSignals
+using FFTW
 
 sampling_freq = 15e6Hz
 num_samples = 20000
@@ -18,9 +19,23 @@ for type in [Float64, Float32, Int16, Int32]
         else
             signal_typed = Complex{type}.(signal)
         end
+        acq_plan = AcquisitionPlan(
+            system,
+            num_samples,
+            sampling_freq;
+            prns = 1:1,
+            fft_flag = FFTW.ESTIMATE,
+        )
         SUITE["Acquire"][type][system_type] =
-            @benchmarkable acquire($system, $signal_typed, $sampling_freq, $1)
+            @benchmarkable acquire!($acq_plan, $signal_typed, $(1:1))
+        coarse_fine_acq_plan = CoarseFineAcquisitionPlan(
+            system,
+            num_samples,
+            sampling_freq;
+            prns = 1:1,
+            fft_flag = FFTW.ESTIMATE,
+        )
         SUITE["Coarse Fine Acquire"][type][system_type] =
-            @benchmarkable coarse_fine_acquire($system, $signal_typed, $sampling_freq, $1)
+            @benchmarkable acquire!($coarse_fine_acq_plan, $signal_typed, $(1:1))
     end
 end
